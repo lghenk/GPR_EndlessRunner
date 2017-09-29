8be12703b0e34249d31b9738039509a1b31e5d77
samjerry <jerry.sam@ziggo.nl>
2017-09-29 13:24:56 +0200

    less cancer

---
+++ Assets/Scripts/Managers/DeathManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathManager : MonoBehaviour {
+
+	public int startingHealth = 3;
+	public int currentHealth;
+
+	bool isDead;
+	bool damaged;
+
+
+	void Awake (){
+		currentHealth = startingHealth;
+	}
+		
+	void Update ()
+	{
+		if (damaged) {
+			
+		}
+		damaged = false;
+	}
+
+	public void TakeDamage (int amount)
+		{
+			damaged = true;
+
+			currentHealth -= 1;
+
+			if(currentHealth <= 0 && !isDead)
+			{
+				Death ();
+			}
+		}
+
+
+		void Death ()
+		{
+			isDead = true;
+			Time.timeScale = 0;
+		}
+	}
---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreManager : MonoBehaviour {
+
+	public static float score;
+	Text text;
+
+	void Awake(){
+		text = GetComponent<Text>();
+		score = 0;
+	}
+
+	void Start () {
+		InvokeRepeating("updateScore",0.000015f, 0.000015f);
+	}
+
+	void Update(){
+		text.text = "Meters: " + score;
+	}
+
+	void updateScore(){
+		score++;
+		Debug.Log(score);
+	}
+
+}
