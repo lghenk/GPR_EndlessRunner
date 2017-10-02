a1d2d1bdc8deaf283b04b2e61289bbfb13b9a077
Timo <timo.heijne@kpnplanet.nl>
2017-10-02 13:45:12 +0200

    Fixed Pauze for jerry

--- Assets/Scripts/Managers/PauseManager.cs
+++ Assets/Scripts/Managers/PauseManager.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseManager : MonoBehaviour {
-
 	private bool isPaused = false;
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Escape) && !isPaused) {
-			isPaused = true;
-			Time.timeScale = 0;
-		} else {
-			isPaused = false;
-			Time.timeScale = 1;
-		}
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if(isPaused) {
+                isPaused = false;
+                Time.timeScale = 1;
+                Debug.Log("Game Resumed");
+            } else {
+                isPaused = true;
+                Time.timeScale = 0;
+                Debug.Log("Game Pauzed");
+            }
+        }
 	}
 }
