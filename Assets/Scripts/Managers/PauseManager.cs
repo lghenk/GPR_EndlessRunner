155954a5edf515899e15daa1da960036da443496
samjerry <jerry.sam@ziggo.nl>
2017-10-12 11:31:30 +0200

    fixed some shiz

--- Assets/Scripts/Managers/PauseManager.cs
+++ Assets/Scripts/Managers/PauseManager.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseManager : MonoBehaviour {
+
 	private bool isPaused = false;
-	// Update is called once per frame
+
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            if(isPaused) {
-                isPaused = false;
-                Time.timeScale = 1;
-                Debug.Log("Game Resumed");
-            } else {
-                isPaused = true;
-                Time.timeScale = 0;
-                Debug.Log("Game Pauzed");
-            }
-        }
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (isPaused) {
+				isPaused = false;
+				Time.timeScale = 1;
+				Debug.Log ("Game resumed");
+			} else {
+				isPaused = true;
+				Time.timeScale = 0;
+				Debug.Log ("Game p");
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/UI/MenuBlink.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuBlink : MonoBehaviour {
+
+	public RawImage quit;
+	private RawImage image;
+	private bool isActive = true;
+
+	void Start ()
+	{
+		image = GetComponent<RawImage>();
+		StartBlinking();
+	}
+
+	IEnumerator Blink()
+	{
+		while (true) {
+			if (!isActive) {
+				image.enabled = false;
+				quit.enabled = true;
+				yield return new WaitForSecondsRealtime(.5f);
+			} else {
+				image.enabled = true;
+				quit.enabled = false;
+				yield return new WaitForSecondsRealtime(.80f);
+			}
+
+			isActive = !isActive;
+		}
+	}
+
+	void StartBlinking()
+	{
+		StopAllCoroutines();
+		StartCoroutine("Blink");
+	}
+
+	 public void StopBlinking()
+	{
+		StopCoroutine(Blink());
+	}
+}
